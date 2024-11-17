6d2498452ee91f908050b929378a65e743c0be07
adamlaycock <adam.m.laycock@gmail.com>
2024-11-17 21:24:22 +0000

    add mitochondrial genes and used copy method to fix slice error

--- EDA Script.py
+++ EDA Script.py
@@ -17,30 +17,87 @@ gene_df['name'] = gene_df['name'].astype('str')
 
 # Hypothesis: 'The expression of mitochondrial replication genes shows less periodicity compared to the expression nuclear DNA replication genes'
 
-topoiso_lst = ['TOP1', 'TOP2', 'TOP3']
-topoisomerases = gene_df[gene_df['symbol'].isin(topoiso_lst)]
-topoisomerases['complex'] = 'Topoisomerase'
+# Nuclear DNA Replicated Related Genes
+topoiso_lst = [
+    'TOP1', 
+    'TOP2', 
+    'TOP3'
+]
+topiso = gene_df[gene_df['symbol'].isin(topoiso_lst)].copy()
+topiso.loc[:, 'complex'] = 'Topoisomerase'
 
-dna_heli_lst = ['HCS1']
-helicase = gene_df[gene_df['symbol'].isin(dna_heli_lst)]
-helicase['complex'] = 'DNA Helicase'
+dna_heli_lst = [
+    'HCS1'
+]
+helicase = gene_df[gene_df['symbol'].isin(dna_heli_lst)].copy()
+helicase.loc[:, 'complex'] = 'DNA Helicase'
 
-ligase_lst = ['DNL4']
-ligase = gene_df[gene_df['symbol'].isin(ligase_lst)]
-ligase['complex'] = 'DNA Ligase'
+ligase_lst = [
+    'CDC9'
+]
+ligase = gene_df[gene_df['symbol'].isin(ligase_lst)].copy()
+ligase.loc[:, 'complex'] = 'DNA Ligase'
 
-pol_ep_lst = ['DPB2', 'DPB3', 'DPB4']
-pol_ep = gene_df[gene_df['symbol'].isin(pol_ep_lst)]
-pol_ep['complex'] = 'Pol Epsilon'
+pol_ep_lst = [
+    'DPB2', 
+    'DPB3', 
+    'DPB4'
+]
+pol_ep = gene_df[gene_df['symbol'].isin(pol_ep_lst)].copy()
+pol_ep.loc[:, 'complex'] = 'Pol Epsilon'
 
-pol_al_lst = ['POL1', 'POL12', 'PRI2', 'PRI1']
-pol_al = gene_df[gene_df['symbol'].isin(pol_al_lst)]
-pol_al['complex'] = 'Pol Alpha'
+pol_al_lst = [
+    'POL1', 
+    'POL12', 
+    'PRI2', 
+    'PRI1'
+]
+pol_al = gene_df[gene_df['symbol'].isin(pol_al_lst)].copy()
+pol_al.loc[:, 'complex'] = 'Pol Alpha'
 
-pol_de_lst = ['POL3', 'POL31', 'POL32']
-pol_de = gene_df[gene_df['symbol'].isin(pol_de_lst)]
-pol_de['complex'] = 'Pol Delta'
+pol_de_lst = [
+    'POL3', 
+    'POL31', 
+    'POL32'
+]
+pol_de = gene_df[gene_df['symbol'].isin(pol_de_lst)].copy()
+pol_de.loc[:, 'complex'] = 'Pol Delta'
 
-telo_lst = ['EST1', 'EST2', 'EST3']
-telo = gene_df[gene_df['symbol'].isin(telo_lst)]
-telo['complex'] = 'Telomerase'
+telo_lst = [
+    'EST1', 
+    'EST2', 
+    'EST3'
+]
+telo = gene_df[gene_df['symbol'].isin(telo_lst)].copy()
+telo.loc[:, 'complex'] = 'Telomerase'
+
+nuclear_df = pd.concat([topiso, helicase, ligase, pol_ep, pol_al, pol_de, telo])
+
+# Mitochondrial Replication Related Genes
+mt_ribo_lst = [
+    'VAR1'
+]
+mt_ribo = gene_df[gene_df['symbol'].isin(mt_ribo_lst)].copy()
+mt_ribo.loc[:, 'complex'] = 'Mitoribosomes'
+
+pol_ga_lst = [
+    'MIP1'
+]
+pol_ga = gene_df[gene_df['symbol'].isin(pol_ga_lst)].copy()
+pol_ga.loc[:, 'complex'] = 'Pol Gamma'
+
+atp_syn_lst = [
+    "ATP1", "ATP3", "ATP7", "ATP16", "ATP5", "ATP17", "ATP12", 
+    "ATP2", "ATP14", "ATP10", "ATP11", "ATP4", "ATP15", "ATP20", 
+    "ATP18", "ATP8", "ATP6", "ATP19"
+]
+atp_syn = gene_df[gene_df['symbol'].isin(atp_syn_lst)].copy()
+atp_syn.loc[:, 'complex'] = 'ATP Synthase'
+
+cco_lst = [
+    "COX15", "COX6", "COX5B", "COX9", "COX20", "COX4", "COX13",
+    "COX18", "COX16", "COX17", "COX12", "COX8", "COX14", "COX7",
+    "COX5A", "COX11", "COX10", "COX19", "COX1", "COX2", "COX3"
+]
+cco_lst = gene_df[gene_df['symbol'].isin(cco_lst)].copy()
+cco_lst.loc[:, 'complex'] = 'Cytochrome C Oxidase'
